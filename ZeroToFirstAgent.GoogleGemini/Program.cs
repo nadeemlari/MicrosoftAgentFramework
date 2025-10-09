9f27d0eff298940a78d61c634e5cf8b04ab14880
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-10-09 19:38:21 +0530

    Add ZeroToFirstAgent.GoogleGemini project with initial implementation

---
+++ ZeroToFirstAgent.GoogleGemini/Program.cs
@@ -0,0 +1,18 @@
+﻿using GenerativeAI;
+using GenerativeAI.Microsoft;
+using Microsoft.Agents.AI;
+using Shared;
+
+
+const string apiKey = "";
+const string model = GoogleAIModels.Gemini2Flash;        
+var client = new GenerativeAIChatClient(apiKey, model);
+var agent = new ChatClientAgent(client);
+var response = await agent.RunAsync("What is the Capital of Australia?");
+Utils.Separator();
+Utils.WriteLineSuccess(response.Text);
+Utils.Separator();
+await foreach (AgentRunResponseUpdate update in agent.RunStreamingAsync("How to make soup?"))
+{
+    Utils.WriteYellow(update.Text);
+}
