135dcf7779d2ca0781c5b6ff964909e609e7a473
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-10-09 20:55:29 +0530

    Add ZeroToFirstAgent.OpenAi project with initial implementation and example usage

---
+++ ZeroToFirstAgent.OpenAi/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Agents.AI;
+using OpenAI;
+using Shared;
+
+const string apiKey = "....";
+const string model = "gpt-4.1-mini";
+var client = new OpenAIClient(apiKey);
+var agent = client.GetChatClient(model).CreateAIAgent();
+var response = await agent.RunAsync("What is the capital of Germany?");
+ConsoleUtils.Separator();
+ConsoleUtils.WriteLineYellow(response.Text);
+ConsoleUtils.Separator();
+await foreach (AgentRunResponseUpdate message in agent.RunStreamingAsync("How to make soup?"))
+{
+    ConsoleUtils.WriteSuccess(message.Text);
+}
