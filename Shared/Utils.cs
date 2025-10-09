2b82777bac7aa6473402d1065a9d1bef0b401c1b
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-10-09 19:19:09 +0530

    Implement chat client agent functionality and enhance console output methods

--- Shared/Utils.cs
+++ Shared/Utils.cs
@@ -46,6 +46,50 @@ public static class Utils
         }
     }
 
+    public static void WriteError(Exception e)
+    {
+        WriteError(e.ToString());
+    }
+
+    public static void WriteError(string text)
+    {
+        Write(text, ConsoleColor.Red);
+    }
+
+    public static void WriteYellow(string text)
+    {
+        Write(text, ConsoleColor.Yellow);
+    }
+
+    public static void WriteWarning(string text)
+    {
+        Write(text, ConsoleColor.Yellow);
+    }
+
+    public static void WriteInformation(string text)
+    {
+        Write(text, ConsoleColor.DarkGray);
+    }
+
+    public static void WriteSuccess(string text)
+    {
+        Write(text, ConsoleColor.Green);
+    }
+
+    public static void Write(string text, ConsoleColor color)
+    {
+        ConsoleColor orgColor = Console.ForegroundColor;
+        try
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+        }
+        finally
+        {
+            Console.ForegroundColor = orgColor;
+        }
+    }
+    
     public static void Separator()
     {
         Console.WriteLine();
--- ZeroToFirstAgent.AnthropicClaude/Program.cs
+++ ZeroToFirstAgent.AnthropicClaude/Program.cs
@@ -1,5 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using Anthropic.SDK;
+using Anthropic.SDK.Constants;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
 using Shared;
+
+const string apiKey = "...";
+const string model = AnthropicModels.Claude3Haiku;
+var client = new AnthropicClient(apiKey).Messages.AsBuilder().Build();
+var chatClientAgentRunOptions = new ChatClientAgentRunOptions( new ChatOptions
+{
+    ModelId = model,
+    MaxOutputTokens = 1000
+});
+var agent = new ChatClientAgent(client);
+var response = await agent.RunAsync("What is the Capital of Australia?", options:chatClientAgentRunOptions);
+Utils.WriteLineSuccess(response.Text);
 Utils.Separator();
-Utils.WriteLineSuccess("ZeroToFirstAgent.AnthropicClaude");
+await foreach (AgentRunResponseUpdate update in agent.RunStreamingAsync("How to make soup?", options: chatClientAgentRunOptions))
+{
+    Utils.WriteYellow(update.Text);
+}
