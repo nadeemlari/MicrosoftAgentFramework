5f0b9150ceacfda9e014e9f9946bcae9fd04aec0
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-10-16 12:37:06 +0530

    Add ConversationThreads project with agent thread persistence and structured output models

---
+++ ConversationThreads/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using ConversationThreads;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using Microsoft.VisualBasic.CompilerServices;
+using OpenAI;
+using Shared;
+
+var client = new AzureOpenAIClient(new Uri("https://nad-openai-azure.openai.azure.com/"), new AzureCliCredential());
+var agent= client.GetChatClient("GPT-4.1-mini").CreateAIAgent("You are a Friendly AI Bot, answering questions");
+
+var thread = agent.GetNewThread();
+const bool optionToResume = true;
+if (optionToResume)
+{
+    thread = await AgentThreadPersistence.ResumeChatIfRequestedAsync(agent);
+}
+
+while (true)
+{
+    Console.Write("> ");
+    var input = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(input))
+    {
+        var message = new ChatMessage(ChatRole.User, input);
+        await foreach (AgentRunResponseUpdate update in agent.RunStreamingAsync(message, thread))
+        {
+            Console.Write(update);
+        }
+    }
+
+    DisplayUtil.Separator();
+
+    if (optionToResume)
+    {
+        await AgentThreadPersistence.StoreThreadAsync(thread);
+    }
+}
---
+++ StructuredOutput/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using Shared;
+using StructuredOutput.Models;
+
+var client = new AzureOpenAIClient(new Uri("https://nad-openai-azure.openai.azure.com/"), new AzureCliCredential());
+var question = "What are the top 10 Movies according to IMDB?";
+
+//Without Structured Output
+var agent1 = client
+    .GetChatClient("GPT-4.1-mini")
+    .CreateAIAgent(instructions: "You are an expert in IMDB Lists");
+
+var response1 = await agent1.RunAsync(question);
+DisplayUtil.Separator();
+DisplayUtil.WriteLineInformation(response1.Text);
+
+//With Structured Output
+var agent2 = client 
+    .GetChatClient("GPT-4.1-mini")
+    .CreateAIAgent(instructions: "You are an expert in IMDB Lists");
+
+var response2 = await agent2.RunAsync<MovieResult>(question);
+var movieResult2 = response2.Result;
+DisplayUtil.Separator();
+DisplayMovies(movieResult2);
+
+//More cumbersome but sometimes needed way
+var jsonSerializerOptions = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+    TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+    Converters = { new JsonStringEnumConverter() }
+};
+
+var agent3 = client
+    .GetChatClient("GPT-4.1-mini")
+    .CreateAIAgent(instructions: "You are an expert in IMDB Lists");
+
+var chatResponseFormatJson = ChatResponseFormat.ForJsonSchema<MovieResult>(jsonSerializerOptions);
+
+var response3 = await agent3.RunAsync(question, options: new ChatClientAgentRunOptions()
+{
+    ChatOptions = new ChatOptions
+    {
+        ResponseFormat = chatResponseFormatJson
+    }
+});
+
+var movieResult3 = response3.Deserialize<MovieResult>(jsonSerializerOptions);
+DisplayUtil.Separator();
+DisplayMovies(movieResult3);
+return;
+
+
+void DisplayMovies(MovieResult movieResult)
+{
+    var counter = 1;
+    Console.WriteLine(movieResult!.MessageBack);
+    foreach (var movie in movieResult.Top10Movies)
+    {
+        DisplayUtil.WriteLineSuccess($"{counter}: {movie.Title} ({movie.YearOfRelease}) - Genre: {movie.Genre} - Director: {movie.Director} - IMDB Score: {movie.ImdbScore}");
+        counter++;
+    }
+}
