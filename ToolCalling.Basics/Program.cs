c042e9c3f029032b9db36348a42cf26e917c01c3
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-10-22 12:12:31 +0530

    Add ToolCalling.Basics project with utility functions for current date, time, and timezone

---
+++ ToolCalling.Basics/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.AI;
+using MicrosoftAgentFramework.Utilities;
+using OpenAI;
+using Shared;
+using ToolCalling.Basics;
+
+// const string model = "openai/gpt-4.1-mini";
+const string model = "deepseek/deepseek-v3.2-exp";
+var client = AIChatClientProvider.GetOpenAIChatClient(LlmOpenAiProviders.OpenRouter,model);
+var agent = client.CreateAIAgent(
+    instructions: "YYou are a Time Expert",
+    tools: [AIFunctionFactory.Create(Tools.CurrentDataAndTime), AIFunctionFactory.Create(Tools.CurrentTimezone)]
+);
+var thread = agent.GetNewThread();
+while (true)
+{
+    Console.Write("User: ");
+    var userInput = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(userInput))
+        break;
+
+    var message = new ChatMessage(ChatRole.User, userInput);
+    DisplayUtil.LoadingTask();
+    var response = await agent.RunAsync(message, thread);
+    DisplayUtil.StopLoading();
+    DisplayUtil.Separator();
+    DisplayUtil.WriteLineSuccess($"Agent: {response.Text}");
+}
