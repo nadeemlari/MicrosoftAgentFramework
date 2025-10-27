337062e087f55f50e92a853e71b402c6b1e4123b
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-10-27 18:47:54 +0530

    Add AIResponseClientProvider and OpenAIResponsesApi.WebSearch project; implement AI agent creation in Program.cs

---
+++ MicrosoftAgentFramework.Utilities/AIResponseClientProvider.cs
@@ -0,0 +1,43 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System.ClientModel;
+using OpenAI;
+using OpenAI.Responses;
+
+#pragma warning disable OPENAI001
+
+namespace MicrosoftAgentFramework.Utilities;
+
+public static class AIResponseClientProvider
+{
+    public static OpenAIResponseClient GetAIResponseClient(LlmOpenAiProviders provider,  string model)
+    {
+        OpenAIClient client;
+        switch (provider)
+        {
+                
+            case LlmOpenAiProviders.OpenAI:
+                var apiKey = Environment.GetEnvironmentVariable("OpenAI__ApiKey") ??
+                             throw new InvalidOperationException("Please set the OpenAI__ApiKey environment variable.");
+                client = new OpenAIClient(new ApiKeyCredential(apiKey));
+
+                break;
+            case LlmOpenAiProviders.OpenRouter:
+                 apiKey = Environment.GetEnvironmentVariable("OPENROUTER_API_KEY") ??
+                             throw new InvalidOperationException(
+                                 "Please set the OPENROUTER_API_KEY environment variable.");
+                 client = new OpenAIClient(new ApiKeyCredential(apiKey), new OpenAIClientOptions
+                {
+                    Endpoint = new Uri("https://openrouter.ai/api/v1")
+                });
+                break;
+
+            case LlmOpenAiProviders.AzureOpenAI:
+            case LlmOpenAiProviders.A4F:
+            default:
+                throw new ArgumentOutOfRangeException(nameof(provider), provider, null);
+        }
+
+        return client.GetOpenAIResponseClient(model);
+    }
+}
---
+++ OpenAIResponsesApi.WebSearch/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using MicrosoftAgentFramework.Utilities;
+using OpenAI;
+using Shared.Extensions;
+
+const string model = "openai/gpt-4.1";
+// const string model = "gpt-4.1";
+var resClient = AIResponseClientProvider.GetAIResponseClient(LlmOpenAiProviders.OpenRouter, model);
+
+var agent = resClient.CreateAIAgent(
+    instructions: "You are a Space News AI Reporter",
+    tools: [new HostedWebSearchTool()]
+);
+
+var question = "What is today's news in Space Exploration (List today's date at the top)";
+List<AgentRunResponseUpdate> updates = [];
+await foreach (var update in agent.RunStreamingAsync(question))
+{
+    updates.Add(update);
+    Console.Write(update);
+}
+var fullResponse = updates.ToAgentRunResponse();
+fullResponse.Usage.OutputAsInformation();
