fa5d06cf332b4f597965486a3680a7068a956a8f
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-10-29 19:29:31 +0530

    Add OpenAIResponsesApi.SpecialModels project; implement AI agent functionality in Program.cs

---
+++ OpenAIResponsesApi.SpecialModels/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Agents.AI;
+using MicrosoftAgentFramework.Utilities;
+using OpenAI;
+using Shared.Extensions;
+
+const string model = "gpt-5-codex";
+var agent = AIResponseClientProvider
+    .GetAIResponseClient(OpenAI_LLM_Providers.OpenAI, model)
+    .CreateAIAgent(instructions: "You are a C# Developer");
+List<AgentRunResponseUpdate> updates = [];
+var question = "Show me an C# Example of a method adding two numbers";
+await foreach (var update in agent.RunStreamingAsync(question))
+{
+    updates.Add(update);
+    Console.Write(update);
+}
+
+var fullResponse = updates.ToAgentRunResponse();
+fullResponse.Usage.OutputAsInformation();
