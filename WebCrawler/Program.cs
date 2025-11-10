a3402a49b1fc7d449719a1b853f7022cdf2d3036
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-11-10 13:04:52 +0530

    Comment out PDF ingestion code in Program.cs

--- WebCrawler/Program.cs
+++ WebCrawler/Program.cs
@@ -17,12 +17,12 @@ var apiKey = Environment.GetEnvironmentVariable("OpenAI__ApiKey")??"your-api-key
 
 #region PDF Ingestion
 
-var vectorStore = new QdrantVectorStore(new QdrantClient("qdrant.pub.nadeemlari.in"), ownsClient: true);
-var chunksCollection = vectorStore.GetCollection<ulong, PdfChunk>("solenis_pdf_chunk_content");
-var documentsCollection = vectorStore.GetCollection<ulong, IngestedPdfDocument>("solenis_pdf_doc_content");
-
-var embeddingService = new OpenAITextEmbeddingGenerationService(modelId: "text-embedding-3-small", apiKey: apiKey);
-
-var pdfIngestor = new PdfIngestor(chunksCollection, documentsCollection);
-await pdfIngestor.IngestDataAsync(new PdfDirectorySource(@"C:\Users\mnadeem\RiderProjects\MicrosoftAgentFramework\WebCrawler\Data\Pdfs", embeddingService));
+// var vectorStore = new QdrantVectorStore(new QdrantClient("qdrant.pub.nadeemlari.in"), ownsClient: true);
+// var chunksCollection = vectorStore.GetCollection<ulong, PdfChunk>("solenis_pdf_chunk_content");
+// var documentsCollection = vectorStore.GetCollection<ulong, IngestedPdfDocument>("solenis_pdf_doc_content");
+//
+// var embeddingService = new OpenAITextEmbeddingGenerationService(modelId: "text-embedding-3-small", apiKey: apiKey);
+//
+// var pdfIngestor = new PdfIngestor(chunksCollection, documentsCollection);
+// await pdfIngestor.IngestDataAsync(new PdfDirectorySource(@"C:\Users\mnadeem\RiderProjects\MicrosoftAgentFramework\WebCrawler\Data\Pdfs", embeddingService));
 #endregion
