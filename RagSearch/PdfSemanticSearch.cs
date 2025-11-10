33d7eea200bcb5eca3cfd12c4e9b65ab9b29b5da
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-11-10 17:07:09 +0530

    Add PdfSemanticSearch class and update Program.cs for PDF search functionality

---
+++ RagSearch/PdfSemanticSearch.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.VectorData;
+using Microsoft.SemanticKernel.Connectors.Qdrant;
+using MicrosoftAgentFramework.Utilities;
+using OpenAI.Embeddings;
+using Qdrant.Client;
+using RagUtils;
+
+namespace RagSearch;
+
+public class PdfSemanticSearch
+{
+    
+    private  readonly EmbeddingClient _embeddingClient = OpenAIClientProvider.GetOpenAIClient(OpenAI_LLM_Providers.OpenAI).GetEmbeddingClient("text-embedding-3-small");
+    
+    private readonly VectorStore _vectorStore =
+        new QdrantVectorStore(new QdrantClient("qdrant.pub.nadeemlari.in"), ownsClient: true);
+
+    public async Task<List<(PdfChunk record, double? Score)>> SearchAsync(
+        string query,
+        int topK = 5)
+    {
+        var collection = _vectorStore.GetCollection<ulong, PdfChunk>("solenis_pdf_chunk_content");
+        var x = await _embeddingClient.GenerateEmbeddingAsync(query);
+        
+        var searchResults = collection.SearchAsync(x.Value.ToFloats(), 
+            topK
+        );
+
+        var results = new List<(PdfChunk, double?)>();
+        await foreach (var result in searchResults)
+        {
+            results.Add((result.Record, result.Score));
+        }
+
+        return results;
+    }
+}
--- RagSearch/Program.cs
+++ RagSearch/Program.cs
@@ -2,17 +2,32 @@
 using Shared;
 
 
-DisplayUtil.WriteLineInformation("Web  Search - Web Semantic Search Example - Write Your Query");
-var query = Console.ReadLine();
-var webSearch = new WebSemanticSearch();
-var results = await webSearch.SearchAsync(query ?? string.Empty, topK: 5);
+// DisplayUtil.WriteLineInformation("Web  Search - Web Semantic Search Example - Write Your Query");
+// var query = Console.ReadLine();
+// var webSearch = new WebSemanticSearch();
+// var results = await webSearch.SearchAsync(query ?? string.Empty, topK: 5);
+//
+// DisplayUtil.Separator();
+// foreach (var (record, score) in results)
+// {
+//     DisplayUtil.WriteLineInformation($"Title: {record.Title}");
+//     DisplayUtil.WriteLineInformation($"URL: {record.Url}");
+//     DisplayUtil.WriteLineInformation($"Score: {score}");
+//     DisplayUtil.WriteLineInformation($"Content Snippet: {record.Content[..Math.Min(200, record.Content.Length)]}...");
+//     DisplayUtil.Separator();
+// }
+
+DisplayUtil.WriteLineInformation("Pdf  Search - Pdf Semantic Search Example - Write Your Query");
+var pdfQuery = Console.ReadLine();
+var pdfSearch = new PdfSemanticSearch();
+var pdfResults = await pdfSearch.SearchAsync(pdfQuery ?? string.Empty, topK: 5);
 
 DisplayUtil.Separator();
-foreach (var (record, score) in results)
+foreach (var (record, score) in pdfResults)
 {
-    DisplayUtil.WriteLineInformation($"Title: {record.Title}");
-    DisplayUtil.WriteLineInformation($"URL: {record.Url}");
+    DisplayUtil.WriteLineInformation($"Title: {record.DocumentId}");
+    DisplayUtil.WriteLineInformation($"Page#: {record.PageNumber}");
     DisplayUtil.WriteLineInformation($"Score: {score}");
-    DisplayUtil.WriteLineInformation($"Content Snippet: {record.Content[..Math.Min(200, record.Content.Length)]}...");
+    DisplayUtil.WriteLineInformation($"Content Snippet: {record.Text[..Math.Min(200, record.Text.Length)]}...");
     DisplayUtil.Separator();
 }
