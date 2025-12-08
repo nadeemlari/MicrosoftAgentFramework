cf384d109a44f39fb77fdd569ffc8d6a48d745cb
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-12-08 18:50:56 +0530

    Add SpeechToText project and implement audio recording and transcription functionality

---
+++ SpeechToText/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ClientModel;
+using NAudio.Utils;
+using NAudio.Wave;
+using OpenAI;
+using OpenAI.Audio;
+using Shared;
+
+var apiKey = Environment.GetEnvironmentVariable("OpenAI__ApiKey") ;
+var client = new OpenAIClient(apiKey);
+var audioClient = client.GetAudioClient("gpt-4o-mini-tts");
+var agent = client
+    .GetChatClient("gpt-4.1-mini")
+    .CreateAIAgent(instructions: "You are a Friendly AI Bot, answering questions");
+
+var thread =  agent.GetNewThread();
+
+while (true)
+{
+    Console.WriteLine("Press any key to start recording...");
+    Console.ReadKey();
+    using var audioStream = RecordAudio();
+    ClientResult<AudioTranscription> result = await audioClient.TranscribeAudioAsync(audioStream, "audio.wav");
+    var questionFromAudio = result.Value.Text;
+    Console.WriteLine($"> {questionFromAudio}");
+    var response = await agent.RunAsync(questionFromAudio, thread);
+    Console.WriteLine(response);
+    
+    DisplayUtil.Separator();
+}
+
+MemoryStream RecordAudio()
+{
+    MemoryStream stream = new();
+    using WaveInEvent waveIn = new();
+    waveIn.WaveFormat = new WaveFormat(16000, 16, 1);
+    WaveFileWriter writer = new(new IgnoreDisposeStream(stream), waveIn.WaveFormat);
+
+    waveIn.DataAvailable += (_, args) => { writer.Write(args.Buffer, 0, args.BytesRecorded); };
+    waveIn.StartRecording();
+
+    Console.WriteLine("Recording... Press any key to stop");
+    Console.ReadKey();
+
+    waveIn.StopRecording();
+    stream.Position = 0;
+    return stream;
+}
