6eba83b806740f396e6574e459e2357a99435f6a
Mohammad Nadeem <mnadeem@contractor.solenis.com>
2025-12-11 12:40:49 +0530

    Add ImageGeneration project and implement image generation functionality using OpenAI API

---
+++ ImageGeneration.OpenAi/Program.cs
@@ -0,0 +1,25 @@
+﻿using MicrosoftAgentFramework.Utilities;
+using OpenAI.Images;
+using System.Diagnostics;
+#pragma warning disable OPENAI001
+
+var client = ImageClientProvider.GetOpenAiClient("gpt-image-1");
+var image = await client.GenerateImageAsync("A Tiger in a jungle with a party-hat", new ImageGenerationOptions{
+    
+    Background = GeneratedImageBackground.Auto,
+    Quality = GeneratedImageQuality.Auto,
+    Size = GeneratedImageSize.W1024xH1024,
+    OutputFileFormat = GeneratedImageFileFormat.Png,
+});
+var bytes = image.Value.ImageBytes.ToArray();
+var path = Path.Combine(Path.GetTempPath(), $"image-{Guid.NewGuid():N}.png");
+File.WriteAllBytes(path, bytes);
+
+await Task.Factory.StartNew(() =>
+{
+    Process.Start(new ProcessStartInfo
+    {
+        FileName = path,
+        UseShellExecute = true
+    });
+});
